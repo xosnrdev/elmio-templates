356edc74da590c8d7a9317e0ee8fab172099ec79
xosnrdev <successxodev@gmail.com>
2024-11-16 08:43:27 -0500

    not needed

--- elmio-tailwind/elmio-tailwind-web/test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it } from "vitest";
-
-import { add } from "../src/index";
-
-it("should add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-    expect(add(2, 3)).toBe(5);
-});
