1f960ff2d1120e8c64720c6e32faadcffb15cd7b
xosnrdev <successxodev@gmail.com>
2024-11-17 14:22:23 -0500

    Add post_build.sh script for generating HTML files

---
+++ elmio-tailwind/post_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+env="$1"
+event="$2"
+target="debug"
+
+if [[ "$env" == "release" ]]; then
+    target="release"
+fi
+
+if [[ "$event" == "after_asset_hash" || "$env" == "dev" ]]; then
+    # Generate html
+    ./target/$target/app_cli home_page >dist/index.html
+    echo "Not found" >dist/404.html
+fi
